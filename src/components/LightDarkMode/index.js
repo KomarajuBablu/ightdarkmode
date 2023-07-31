dc4f6b455cef34f482289ac341f184a21de8a06f
Komaraju venkata krishna vamsi <Enter you email ID here>
2023-07-31 08:10:11 +0000

    error

---
+++ src/components/LightDarkMode/index.js
@@ -0,0 +1,14 @@
+import {Component} from 'react'
+
+class LightDarkMode extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Click To Change Mode</h1>
+        <button type="button">Light Mode</button>
+      </div>
+    )
+  }
+}
+
+export default LightDarkMode
